98fcfb726763d3e25ba8d4c45efd220eeae9ad85
Luna <lunatic_ann@hotmail.com>
2019-09-02 18:17:11 -0700

    make sure we can sync

---
+++ Assets/Resources/Scripts/TestingScript.cs
@@ -0,0 +1,12 @@
+﻿// make sure our projects syncs with each other
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingScript : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAroundLocal(Vector3.up, 0.1f);
+	}
+}
